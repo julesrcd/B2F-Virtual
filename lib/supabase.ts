24ef79b04846a340c8be98e9e19fcc3b501df541
julesrcd <julesricaud.2011@gmail.com>
2026-06-13 12:20:50 +0200

    correction login

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,13 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-)
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+    },
+  }
+);
